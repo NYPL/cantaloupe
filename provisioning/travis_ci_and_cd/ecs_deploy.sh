f59a55aa890c982b04f8ef7d82e50699f852e8e9
nodanaonlyzuul <beholdthepanda@gmail.com>
2019-01-11 11:37:21 -0500

    Add script to deploy & restart cantaloupe ECS services

---
+++ provisioning/travis_ci_and_cd/ecs_deploy.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+# Deploy only if it's not a pull request
+if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  # Deploy only if we're testing the master branch
+  if [ "$TRAVIS_BRANCH" == "qa" ] || [ "$TRAVIS_BRANCH" == "production" ]; then
+
+    case "$TRAVIS_BRANCH" in
+      production)
+        export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_PRODUCTION
+        export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_PRODUCTION
+        CLUSTER_NAME=cantaloupe-production
+        WEB_APP_SERVICE_NAME="cantaloupe-production"
+        ;;
+      qa)
+        export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_PRODUCTION
+        export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_PRODUCTION
+        CLUSTER_NAME=cantaloupe-production
+        WEB_APP_SERVICE_NAME="cantaloupe-qa"
+        ;;
+    esac
+
+    echo "Deploying $TRAVIS_BRANCH"
+    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION aws ecs update-service --cluster $CLUSTER_NAME --region us-east-1 --service $WEB_APP_SERVICE_NAME --force-new-deployment
+    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION aws ecs update-service --cluster $CLUSTER_NAME --region us-east-1 --service $WORKER_SERVICE_NAME --force-new-deployment
+    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION aws ecs update-service --cluster $CLUSTER_NAME --region us-east-1 --service $IMPORT_WORKER_SERVICE_NAME --force-new-deployment
+  else
+    echo "Skipping deploy because it's not a deployable branch"
+  fi
+else
+  echo "Skipping deploy because it's a PR"
+fi
