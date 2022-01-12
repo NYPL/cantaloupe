a66af11f23d354e8fd658fa9bcfaa330e30b6796
Kristopher Kelly <kkelly219@gmail.com>
2022-01-12 12:44:07 -0500

    [DR-1656] Adjusting local dev ip to point to dockerized cantaloupe instead of dev iiif.

--- nginx-configs/image_server_to_iiif.js
+++ nginx-configs/image_server_to_iiif.js
@@ -45,7 +45,7 @@ function mapImageServerToIIIF(request, response) {
   var imageUrl = "http://0.0.0.0:8182/iiif/2/"+ identitifier + "/" + crop + "/" + urlSegment +"/0/default.jpg"
   
   // For local development ... 
-  // var imageUrl = "http://172.16.1.195:8182/iiif/2/" + identitifier + "/" + crop + "/" + urlSegment +"/0/default.jpg"
+  // var imageUrl = "http://172.17.0.1:8182/iiif/2/" + identitifier + "/" + crop + "/" + urlSegment +"/0/default.jpg"
   
   //  When DR-1470 is complete, delegates.rb will need to know what derivative type is being requested.
   if (paramsHash['t'] == 'u' || paramsHash['t'] == 'j' || paramsHash['t'] == 's') {
