8be802dd5467a8c0a00e52b3fd65f69b9a6fb98a
Alessandra Vertrees <alessandravertrees@nypl.org>
2022-01-25 12:35:31 -0500

    fix spacing oops

--- delegates.rb
+++ delegates.rb
@@ -220,7 +220,7 @@ class CustomDelegate
     begin
       query =  "SELECT UUID FROM file_store WHERE TYPE in ('j', 's', 'g', 'v', 'q', 'w', 'r', 't') "
       query += "AND FILE_ID = ? AND STATUS = 4 "
-      query += "ORDER BY TYPE = 'j' DESC, TYPE = 's' DESC, TYPE = 'g' DESC ,TYPE = 'v' DESC, TYPE = 'q' DESC, TYPE = 'w' DESC, TYPE = 'r' DESC, TYPE = 't' DESC"
+      query += "ORDER BY TYPE = 'j' DESC, TYPE = 's' DESC, TYPE = 'g' DESC, TYPE = 'v' DESC, TYPE = 'q' DESC, TYPE = 'w' DESC, TYPE = 'r' DESC, TYPE = 't' DESC"
       statement = connection.prepare_statement(query)
       statement.setString(1, context['identifier'])
       results = statement.execute_query
