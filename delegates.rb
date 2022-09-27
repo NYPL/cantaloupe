bc771fdd875319aac36c8908c4bd78022bf307e9
Kristopher Kelly <kkelly219@gmail.com>
2022-09-27 12:34:56 -0400

    [DR-2041] No more default filepath. 404 instead.

--- delegates.rb
+++ delegates.rb
@@ -262,15 +262,13 @@ class CustomDelegate
     end
     
     path = nil
+    
     if not uuid.nil?
       uuid =~ /(....)(....)\-(....)\-(....)\-(....)\-(....)(....)(..)../
       path = "/ifs/prod/repo/#{uuid[0..1]}/#{$1}/#{$2}/#{$3}/#{$4}/#{$5}/#{$6}/#{$7}/#{$8}/#{uuid}"
     end
-    if path.nil? 
-      default_image_path != nil ? default_image_path : "/ifs/prod/repo/FF/FF02/CD3C/93C7/11DD/A1C2/8CF9/9956/CD/FF02CD3C-93C7-11DD-A1C2-8CF99956CD08"
-    else
-      path
-    end
+    
+    path
   end
 
   ##
